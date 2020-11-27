66310550ad567db1a3c35feb7bcc1db85c65086a
THEZIAUNKS-INDONESIA <75087483+THEZIAUNKS-ID@users.noreply.github.com>
2020-11-28 01:17:39 +0700

    Add files via upload

---
+++ al-fatiha.py
@@ -0,0 +1,24 @@
+banner = """
+Surat Al Fatihah
+بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
+bismillāhir-raḥmānir-raḥīm
+Dengan nama Allah Yang Maha Pengasih, Maha Penyayang.
+اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ
+al-ḥamdu lillāhi rabbil-'ālamīn
+Segala puji bagi Allah, Tuhan seluruh alam,
+الرَّحْمٰنِ الرَّحِيْمِۙ
+ar-raḥmānir-raḥīm
+Yang Maha Pengasih, Maha Penyayang,
+مٰلِكِ يَوْمِ الدِّيْنِۗ
+māliki yaumid-dīn
+Pemilik hari pembalasan.
+اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُۗ
+iyyāka na'budu wa iyyāka nasta'īn
+Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami mohon pertolongan.
+اِهْدِنَا الصِّرَاطَ الْمُسْتَقِيْمَ ۙ
+ihdinaṣ-ṣirāṭal-mustaqīm
+Tunjukilah kami jalan yang lurus,
+صِرَاطَ الَّذِيْنَ اَنْعَمْتَ عَلَيْهِمْ ەۙ غَيْرِ الْمَغْضُوْبِ عَلَيْهِمْ وَلَا الضَّاۤلِّيْنَ
+ṣirāṭallażīna an'amta 'alaihim gairil-magḍụbi 'alaihim wa laḍ-ḍāllīn
+(yaitu) jalan orang-orang yang telah Engkau beri nikmat kepadanya; bukan (jalan) mereka yang dimurkai, dan bukan (pula jalan) mereka yang sesat."""
+print(banner)
