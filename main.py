3e8f4d704c46f02c3b0d01f008e0372e2ab48e44
THEZIAUNKS-INDONESIA <75087483+THEZIAUNKS-ID@users.noreply.github.com>
2020-11-28 01:10:38 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os,sys
+
+menu = """
+1.Al-Fatiha 4.Al-ikhlas
+2.An-Nas
+3.Al-Falak"""
+print(menu)
+pil = input("PILIH YANG MANA :")
+if fil == '1':
+   os.system('python an-nas.py')
+elif pil == '2':
+     os.system('python al-falaq.py')
+elif pil =='3':
+     os.system('clear')
+     sys.exit()
+else:
+     sys.exit() 
